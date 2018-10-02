42933f8f3dc14f838322a0bacd145ae67eec7286
ifreeworklancer@gmail.com <ifreeworklancer@gmail.com>
2018-10-02 18:45:52 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+// import jquery from 'jquery';
+// import Flickity from 'flickity';
+
+// window.jQuery = window.$ = jquery;
+
+// import 'flickity/dist/flickity.css';
+
+// (function () {
+
+  
+
+// })(jQuery)
+
